b8b36e2c45728649264118d2f0b81548479f1363
Jeferson Furquim <jeferson.souza@cadis.com.br>
2021-10-05 11:24:04 -0300

    Configurando exportacao de login em CSV

---
+++ MontagemCurriculo/Controllers/InformacoesLoginController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MontagemCurriculo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontagemCurriculo.Controllers
+{
+    public class InformacoesLoginController : Controller
+    {
+
+        private readonly Contexto _cotexto;
+
+        public InformacoesLoginController(Contexto contexto)
+        {
+            _cotexto = contexto;
+        }
+
+
+
+        public async Task<IActionResult> Index()
+        {
+            var usuarioId = HttpContext.Session.GetInt32("UsuarioId");
+
+           
+            return View( await _cotexto.InformacoesLogin.Include(u => u.Usuario).Where(i => i.UsuarioId == usuarioId).ToListAsync());
+        }
+
+        public IActionResult DowloadDados()
+        {
+            var usuarioId = HttpContext.Session.GetInt32("UsuarioId");
+            var dados = _cotexto.InformacoesLogin.Include(u => u.Usuario).Where(i => i.UsuarioId == usuarioId).ToList();
+            StringBuilder arquivo = new StringBuilder();
+
+            arquivo.AppendLine("EnderecoIP;Data;Horario");
+
+            foreach (var item in dados)
+            {
+                arquivo.AppendLine(item.EnderecoIP + ";" + item.Data + ";" + item.Horario);
+
+            }
+
+            return File(Encoding.ASCII.GetBytes(arquivo.ToString()), "text/csv", "dados.csv");
+
+        }
+
+
+    }
+}
--- MontagemCurriculo/Models/Contexto.cs
+++ MontagemCurriculo/Models/Contexto.cs
@@ -15,6 +15,8 @@ namespace MontagemCurriculo.Models
         public DbSet<FormacaoAcademica> FormacoesAcademicas { get; set; }
         public DbSet<Objetivo> Objetivos { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<InformacaoLogin> InformacoesLogin { get; set; }
+
 
 
 
