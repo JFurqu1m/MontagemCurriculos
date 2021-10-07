85a8cc0b87b0e888771ec722c0caadb85a9de9e7
Jeferson Furquim <jeferson.souza@cadis.com.br>
2021-10-07 13:37:39 -0300

    View Component experiencias profissionais

--- MontagemCurriculo/Models/Contexto.cs
+++ MontagemCurriculo/Models/Contexto.cs
@@ -13,6 +13,8 @@ namespace MontagemCurriculo.Models
         public DbSet<Idioma> Idiomas { get; set; }
         public DbSet<TipoCurso> TiposCursos { get; set; }
         public DbSet<FormacaoAcademica> FormacoesAcademicas { get; set; }
+        public DbSet<ExperienciaProfissional> ExperienciasProfissionais { get; set; }
+
         public DbSet<Objetivo> Objetivos { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<InformacaoLogin> InformacoesLogin { get; set; }
